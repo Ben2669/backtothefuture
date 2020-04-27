abdbf3be78863715cc41fe3035a1b335b437a88e
Ben2669 <benjamin.moniotte@gmail.com>
2020-04-27 22:14:56 +0200

    mise en place CSS

--- index.php
+++ index.php
@@ -5,110 +5,110 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>The Eye</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Back to the Future</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container-fluid flex-nowrap bg-secondary">
-    <div class="row half">
-        <div class="col-1 bg-danger">
-        </div>
-        <div class="col-1 bg-danger offset-1">
-        </div>
-        <div class="col-3 bg-danger offset-1">
-        </div>
-        <div class="col-1 bg-danger offset-1">
-        </div>
-        <div class="col-1 bg-danger offset-1">
-        </div>
-        <div class="col-1 bg-danger offset-1">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-1">
-        </div>
-        <div class="col-1 bg-secondary">
-        </div>
-        <div class="col">
-        </div>
-        <div class="col-1 bg-danger">
-        </div>
-        <div class="col-1 bg-warning">
-        </div>
-        <div class="col-1 bg-danger">
-        </div>
-        <div class="col">
-        </div>
-        <div class="col-1 bg-secondary">
-        </div>
-    </div>
-    <div class="row half">
-        <div class="col-3">
-        </div>
-        <div class="col-6 bg-dark container-date">
-            <div class="text-white text-center content-date">
-                DESTINATION TIME
-            </div>
-        </div>
-        <div class="col-3">
-        </div>
-    </div>
-    <div class="row bg-dark separation">
+<main>
+    <section>
+        <div class="line">
+            <div class="column">
 
-    </div>
-    <div class="row half">
-        <div class="col-1">
-        </div>
-        <div class="col-2 bg-secondary">
-        </div>
-        <div class="col-2 bg-danger">
-        </div>
-        <div class="col-1 bg-warning">
-        </div>
-        <div class="col-1 bg-dark">
-        </div>
-        <div class="col-1 bg-warning">
-        </div>
-        <div class="col-2 bg-danger">
-        </div>
-        <div class="col-2 bg-secondary">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-1">
-        </div>
-        <div class="col-2 bg-secondary">
-        </div>
-        <div class="col">
-        </div>
-        <div class="col-1 bg-danger">
-        </div>
-        <div class="col-1 bg-warning">
-        </div>
-        <div class="col-1 bg-dark">
-        </div>
-        <div class="col-1 bg-warning">
-        </div>
-        <div class="col-1 bg-danger">
-        </div>
-        <div class="col">
-        </div>
-        <div class="col-2 bg-secondary">
-        </div>
-    </div>
-    <div class="row half">
-        <div class="col-3">
-        </div>
-        <div class="col-6 bg-dark container-date">
-            <div class="text-white text-center content-date">
-                PRESENT TIME
+            </div>
+            <div class="column">
+
+            </div>
+            <div class="column large">
+
+            </div>
+            <div class="column half">
+
+            </div>
+            <div class="column">
+
+            </div>
+            <div class="column">
+
             </div>
         </div>
-        <div class="col-3">
+        <div class="line double">
+            <div class="column-bis">
+
+            </div>
+            <div class="column-bis">
+
+            </div>
+            <div class="column-bis large">
+
+            </div>
+            <div class="column-bis half spec">
+
+            </div>
+            <div class="column-bis">
+
+            </div>
+            <div class="column-bis">
+
+            </div>
         </div>
+        <div class="line">
+            <div class="timesection">
+
+            </div>
+        </div>
+    </section>
+
+    <div class="separate">
     </div>
-</div>
+
+    <section>
+        <div class="line">
+            <div class="column">
+
+            </div>
+            <div class="column">
+
+            </div>
+            <div class="column large">
+
+            </div>
+            <div class="column half">
+
+            </div>
+            <div class="column">
+
+            </div>
+            <div class="column">
+
+            </div>
+        </div>
+        <div class="line double">
+            <div class="column-bis ter">
+
+            </div>
+            <div class="column-bis ter">
+
+            </div>
+            <div class="column-bis large ter">
+
+            </div>
+            <div class="column-bis half spec">
+
+            </div>
+            <div class="column-bis ter">
+
+            </div>
+            <div class="column-bis ter">
+
+            </div>
+        </div>
+        <div class="line">
+            <div class="timesection">
+
+            </div>
+        </div>
+    </section>
+</main>
 </body>
+
 </html>
