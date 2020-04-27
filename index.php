a835473bbd34aafca4bd4ee70ad5fc24fa75601f
Ben2669 <benjamin.moniotte@gmail.com>
2020-04-27 21:19:24 +0200

    backtothefuture premier jet

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Eye</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container-fluid flex-nowrap bg-secondary">
+    <div class="row half">
+        <div class="col-1 bg-danger">
+        </div>
+        <div class="col-1 bg-danger offset-1">
+        </div>
+        <div class="col-3 bg-danger offset-1">
+        </div>
+        <div class="col-1 bg-danger offset-1">
+        </div>
+        <div class="col-1 bg-danger offset-1">
+        </div>
+        <div class="col-1 bg-danger offset-1">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1">
+        </div>
+        <div class="col-1 bg-secondary">
+        </div>
+        <div class="col">
+        </div>
+        <div class="col-1 bg-danger">
+        </div>
+        <div class="col-1 bg-warning">
+        </div>
+        <div class="col-1 bg-danger">
+        </div>
+        <div class="col">
+        </div>
+        <div class="col-1 bg-secondary">
+        </div>
+    </div>
+    <div class="row half">
+        <div class="col-3">
+        </div>
+        <div class="col-6 bg-dark container-date">
+            <div class="text-white text-center content-date">
+                DESTINATION TIME
+            </div>
+        </div>
+        <div class="col-3">
+        </div>
+    </div>
+    <div class="row bg-dark separation">
+
+    </div>
+    <div class="row half">
+        <div class="col-1">
+        </div>
+        <div class="col-2 bg-secondary">
+        </div>
+        <div class="col-2 bg-danger">
+        </div>
+        <div class="col-1 bg-warning">
+        </div>
+        <div class="col-1 bg-dark">
+        </div>
+        <div class="col-1 bg-warning">
+        </div>
+        <div class="col-2 bg-danger">
+        </div>
+        <div class="col-2 bg-secondary">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1">
+        </div>
+        <div class="col-2 bg-secondary">
+        </div>
+        <div class="col">
+        </div>
+        <div class="col-1 bg-danger">
+        </div>
+        <div class="col-1 bg-warning">
+        </div>
+        <div class="col-1 bg-dark">
+        </div>
+        <div class="col-1 bg-warning">
+        </div>
+        <div class="col-1 bg-danger">
+        </div>
+        <div class="col">
+        </div>
+        <div class="col-2 bg-secondary">
+        </div>
+    </div>
+    <div class="row half">
+        <div class="col-3">
+        </div>
+        <div class="col-6 bg-dark container-date">
+            <div class="text-white text-center content-date">
+                PRESENT TIME
+            </div>
+        </div>
+        <div class="col-3">
+        </div>
+    </div>
+</div>
+</body>
+</html>
