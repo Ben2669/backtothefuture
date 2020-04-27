51bd87fe2e68c69d2a7b785635c38fed3b593c12
Ben2669 <benjamin.moniotte@gmail.com>
2020-04-27 23:34:52 +0200

    solution point 2

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+
+$presentTime = new DateTimeImmutable("now");
+$presentTimeBis = $presentTime->format("M-d-Y A h:i");
+
+$destinationTime = new DateTimeImmutable("2015-10-21 10:30");
+
+$timeInterval = $presentTime->diff($destinationTime);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,47 +21,47 @@
     <section>
         <div class="line">
             <div class="column">
-
+                MONTH
             </div>
             <div class="column">
-
+                DAY
             </div>
             <div class="column large">
-
+                YEAR
             </div>
             <div class="column half">
-
+                <?= $presentTime->format("A");?>
             </div>
             <div class="column">
-
+                HOUR
             </div>
             <div class="column">
-
+                MIN
             </div>
         </div>
         <div class="line double">
             <div class="column-bis">
-
+                <?= $presentTime->format("M");?>
             </div>
             <div class="column-bis">
-
+                <?= $presentTime->format("d");?>
             </div>
             <div class="column-bis large">
-
+                <?= $presentTime->format("Y");?>
             </div>
             <div class="column-bis half spec">
 
             </div>
             <div class="column-bis">
-
+                <?= $presentTime->format("h");?>
             </div>
             <div class="column-bis">
-
+                <?= $presentTime->format("i");?>
             </div>
         </div>
         <div class="line">
             <div class="timesection">
-
+                PRESENT TIME
             </div>
         </div>
     </section>
@@ -64,51 +72,56 @@
     <section>
         <div class="line">
             <div class="column">
-
+                MONTH
             </div>
             <div class="column">
-
+                DAY
             </div>
             <div class="column large">
-
+                YEAR
             </div>
             <div class="column half">
-
+                <?= $destinationTime->format("A");?>
             </div>
             <div class="column">
-
+                HOUR
             </div>
             <div class="column">
-
+                MIN
             </div>
         </div>
         <div class="line double">
             <div class="column-bis ter">
-
+                <?= $destinationTime->format("M");?>
             </div>
             <div class="column-bis ter">
-
+                <?= $destinationTime->format("d");?>
             </div>
             <div class="column-bis large ter">
-
+                <?= $destinationTime->format("Y");?>
             </div>
             <div class="column-bis half spec">
 
             </div>
             <div class="column-bis ter">
-
+                <?= $destinationTime->format("h");?>
             </div>
             <div class="column-bis ter">
-
+                <?= $destinationTime->format("i");?>
             </div>
         </div>
         <div class="line">
             <div class="timesection">
-
+                DESTINATION TIME
             </div>
         </div>
     </section>
 </main>
+
+<div style="font-size: 2em">
+    Il y a <?= $timeInterval->y;?> années, <?= $timeInterval->m;?> mois, <?= $timeInterval->d;?> jours, <?= $timeInterval->h;?> heures et <?= $timeInterval->i;?> minutes qui séparent ces deux dates !
+</div>
+
 </body>
 
 </html>
